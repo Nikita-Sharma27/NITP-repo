ece3569a1bf3d09b1bc6402258d1aaa8629d893d
Anvansh <andianurag277@gmail.com>
2022-02-08 12:26:16 +0530

    deplyment attempt-2

--- College/settings.py
+++ College/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rpgq^&fau-uwv*(mrkg$ekxs$cn+gnn8#*s1kpcoukk@!7r)x$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['college-search-website.herokuapp.com' , '127.0.0.1']
 
 
 # Application definition
